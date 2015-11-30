aecd28bc36e4ffb8c2ec6d7cb006092b3afeb7c0
danmo91 <dan.morain91@gmail.com>
2015-11-30 15:56:28 -0700

    starting over on detail page

--- peep_keep_ui/src/app.js
+++ peep_keep_ui/src/app.js
@@ -7,10 +7,9 @@ import Auth from './services/auth'
 
 import Landing from './pages/landing'
 import Navbar from './components/navbar/navbar'
-import Login from 'pages/login'
-import Signup from 'pages/signup'
-import Home from 'pages/home'
-import Detail from 'pages/detail'
+import Login from './pages/login'
+import Signup from './pages/signup'
+import Home from './pages/home'
 
 const App = React.createClass({
   getInitialState() {
@@ -49,7 +48,6 @@ ReactDOM.render((
       <Route path='login' component={Login} />
       <Route path='signup' component={Signup} />
       <Route path='home' component={Home} />
-      <Route path='detail/:id' component={Detail} />
     </Route>
   </Router>),
 document.getElementById('content'));
