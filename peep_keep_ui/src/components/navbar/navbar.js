bd79f063524449542dcfd0af13615f17cfa2bf81
danmo91 <dan.morain91@gmail.com>
2015-12-01 23:40:15 -0700

    updated navbar states

--- peep_keep_ui/src/components/navbar/navbar.js
+++ peep_keep_ui/src/components/navbar/navbar.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './navbar.scss';
-import {RouteHandler, Link, History} from 'react-router';
+import {RouteHandler, Link, History, State} from 'react-router';
 import Auth from './../../services/auth';
 
+
+
 const Navbar = React.createClass ({
-  mixins: [History],
+  mixins: [History, State],
 
   getDefaultProps() {
     return {
       loggedIn: false,
-      name: ''
+      name: '',
     }
   },
 
@@ -25,26 +27,61 @@ const Navbar = React.createClass ({
     let navLeft;
     let navRight;
 
+    let location = window.location.pathname;
+
+
     if (this.props.loggedIn)
     {
-      navLeft = (
-        <ul className="navbar navbar-left">
-          <li className="dropdown">
-            <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{this.props.name}<span className="caret"></span></a>
-            <ul className="dropdown-menu">
-              <li><a href="#">Action</a></li>
-              <li><a href="#">Another action</a></li>
-              <li><a href="#">Something else here</a></li>
-              <li role="separator" className="divider"></li>
-              <li><a href="#" onClick={this.handleLogout}>Log out</a></li>
-            </ul>
-          </li>
-        </ul>
-      )
 
-      navRight = (
-        <a className="navbar navbar-right" href="#">New</a>
-      )
+      if (location == '/detail') {
+        navLeft = (
+          <Link className="navbar navbar-left" to="/home">Back</Link>
+        )
+        navRight = (
+          <ul className="navbar navbar-right">
+            <li className="dropdown">
+              <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i className="fa fa-ellipsis-v"></i></a>
+              <ul className="dropdown-menu dropdown-left">
+                <li><a href="#">Edit</a></li>
+                <li><a href="#">New Experience</a></li>
+              </ul>
+            </li>
+          </ul>
+        )
+      } else if (location == '/experience') {
+        navLeft = (
+          <Link className="navbar navbar-left" to="/detail">Back</Link>
+        )
+        navRight = (
+          <div className="navbar navbar-right">
+            <a href="#">Edit</a>
+          </div>
+
+        )
+      } else {
+        navLeft = (
+          <ul className="navbar navbar-left">
+            <li className="dropdown">
+              <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{this.props.name}<span className="caret"></span></a>
+              <ul className="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" className="divider"></li>
+                <li><a href="#" onClick={this.handleLogout}>Log out</a></li>
+              </ul>
+            </li>
+          </ul>
+        )
+
+        navRight = (
+          <div className="navbar navbar-right">
+            <a href="#">New</a>
+          </div>
+        )
+      }
+
+
     } else {
 
       navLeft = (
